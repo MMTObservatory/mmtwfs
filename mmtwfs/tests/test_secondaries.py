3035787d65a4ff3275a5640cc20e15c70dd63e95
T. E. Pickering <te.pickering@gmail.com>
2017-03-07 16:26:49 -0500

    add test of secondary exception

--- mmtwfs/tests/test_secondaries.py
+++ mmtwfs/tests/test_secondaries.py
@@ -3,9 +3,20 @@
 
 from ..config import mmt_config
 from ..secondary import SecondaryFactory
+from ..custom_exceptions import WFSConfigException
 
 
 def test_secondaries():
     for s in mmt_config['secondary']:
         sec = SecondaryFactory(secondary=s, test="foo")
         assert(sec.test == "foo")
+
+def test_bogus_secondary():
+    try:
+        sec = SecondaryFactory(secondary="bazz")
+    except WFSConfigException:
+        assert True
+    except Exception as e:
+        assert False
+    else:
+        assert False
