6939365256f87b82da6fe42ced0efd8dcd51726b
T. E. Pickering <te.pickering@gmail.com>
2017-03-22 14:10:55 -0400

    zernike.py - refactor to use analytic slopes and fit all modes

--- mmtwfs/zernike.py
+++ mmtwfs/zernike.py
@@ -47,66 +47,6 @@ def pol2cart(arr):
     return np.array([x, y])
 
 
-def make_radial_matrix(r0, r1=None, norm=True, center=None, dtype=np.float, getxy=False):
-    """
-    Make a matrix of size **(r0, r1)** where the value of each element is the Euclidean
-    distance to **center**. If **center**& is not given, it is assumed to be the middle of the matrix.
-    If **norm** is True (default), the distance is normalized to half the radius, i.e. values
-    will range from [-1, 1] for both axes. If only r0 is given, the matrix will be (r0, r0).
-    If r1 is also given, the matrix will be (r0, r1).
-
-    To make a circular binary mask of (r0, r0), use
-        make_radial_matrix(r0) < 1
-
-    Parameters
-    ----------
-    r0: float or int
-        The width of the mask (and height if r1 == None).
-    r1: float or int or None (default: None)
-        The height of the mask, if provided.
-    norm: bool (default: True)
-        Normalize the distance such that 2/(r0, r1) equales a distance of 1.
-    center: None or two-element list-like (default: None)
-        Sets the origin of the matrix (uses the middle if this is set to None).
-    dtype: np.dtype (default: np.float)
-        Data type of the output matrix
-    getxy: bool (default: False)
-        If True, return x, y values instead of r
-
-    Returns
-    -------
-    if getxy:
-        tuple of (r0, 1) and (1, r1) ndarrays
-    else:
-        np.ndarray of shape (r0, r1)
-    """
-    if not r1:
-        r1 = r0
-    if r0 < 0 or r1 < 0:
-        print("make_radial_matrix(): r0 < 0 or r1 < 0?")
-
-    if center is not None and norm and sum(center)/len(center) > 1:
-        raise ValueError("|center| should be < 1 if norm is set")
-
-    if center is None:
-        if norm:
-            center = (0, 0)
-        else:
-            center = (r0 / 2.0, r1 / 2.0)
-
-    if norm:
-        r0v = np.linspace(-1-center[0], 1-center[0], r0).astype(dtype).reshape(-1, 1)
-        r1v = np.linspace(-1-center[1], 1-center[1], r1).astype(dtype).reshape(1, -1)
-    else:
-        r0v = np.linspace(0-center[0], r0-center[0], r0).astype(dtype).reshape(-1, 1)
-        r1v = np.linspace(0-center[1], r1-center[1], r1).astype(dtype).reshape(1, -1)
-
-    if getxy:
-        return r0v, r1v
-    else:
-        return np.sqrt(r0v**2 + r1v**2)
-
-
 def R_mn(m, n, rho):
     """
     Make radial Zernike polynomial on coordinate grid **rho**.
@@ -165,6 +105,9 @@ def dR_drho(m, n, rho):
     dR_mn = R_mn(m, n, rho) * (rho**2 * (n + 2.) + m) / (rho * (1. - rho**2)) - \
             R_mn(m+1, n+1, rho) * (n + m + 2.) / (1. - rho**2)
 
+    return dR_mn
+
+
 def theta_m(m, phi):
     """
     Calculate angular Zernike mode on coordinate grid **phi**
@@ -207,9 +150,9 @@ def dtheta_dphi(m, phi):
     """
     am = np.abs(m)
     if m >= 0.0:
-        dtheta = -m * np.sin(am * phi)
+        dtheta = -am * np.sin(am * phi)
     else:
-        dtheta = m * np.cos(am * phi)
+        dtheta = am * np.cos(am * phi)
     return dtheta
 
 
@@ -379,7 +322,35 @@ def zernike_noll(j, rho, phi, norm=False):
         Wavefront described by Noll Zernike mode, j. Same shape as rho and phi.
     """
     n, m = noll_to_zernike(j)
-    return zernike(m, n, rho, phi, norm)
+    wf = zernike(m, n, rho, phi, norm)
+    return wf
+
+
+def zernike_slope_noll(j, rho, phi, norm=False):
+    """
+    Calculate X/Y slopes for Noll Zernike mode **j** on grid **rho** and **phi**.
+    **rho** and **phi** must be radial and azimuthal coordinate grids of identical shape, respectively.
+
+    Parameters
+    ----------
+    j: int
+        j-th Noll Zernike index
+    rho: 2D ndarray
+        Radial coordinate grid
+    phi: 2D ndarray
+        Azimuthal coordinate grid
+    norm: bool (default: True)
+        Normalize modes to unit variance (i.e. Noll coefficients)
+
+    Returns
+    -------
+    dwx, dwx: 2D ndarray, 2D ndarray
+        X/Y wavefront slopes of Noll Zernike mode, j. Same shape as rho and phi.
+    """
+    n, m = noll_to_zernike(j)
+    dwx = dZ_dx(m, n, rho, phi, norm=norm)
+    dwy = dZ_dy(m, n, rho, phi, norm=norm)
+    return dwx, dwy
 
 
 def noll_normalization_vector(nmodes=30):
@@ -439,206 +410,40 @@ def noll_coefficient(l):
     return norm_coeff
 
 
-def make_zernike_basis(nmodes, rad, modestart=2, calc_covmat=False):
+def zernike_influence_matrix(pup_coords, nmodes=20, modestart=2):
     """
-    Calculate a basis of **nmodes** Zernike modes with radius **rad**.
-    This output of this function can be used as cache for other functions.
+    Calculate matrices to convert wavefront slopes to Zernike coefficients and to convert Zernike coefficients to
+    wavefront slopes.  This method analytic derivatives to calculate the slopes for Zernike mode.  Adapting this methods
+    for other basis sets would also require calculating the analytic derivatives for those sets.  This method also currently
+    only calculates the slope at the aperture center.  It would be more correct to average over the aperture, but this isn't
+    hugely important for the lower order modes we're most interested in.
 
     Parameters
     ----------
-    nmodes: int
-        Number of modes to generate
-    rad: float
-        Radius of Zernike modes
+    pup_coords: 2-element tuple
+        Pupil coordinates of the aperture centers.
+    nmodes: int (default: 20)
+        Number of Zernike modes to fit.
     modestart: int (default: 2)
-        Noll index of first mode to calculate. By default, start at 2 to ignore the first mode, piston.
-    calc_covmat: bool (default: False)
-        Calculate and return the covariance matrix for Zernike modes and its inverse.
+        First mode to include in the set to fit.
 
     Returns
     -------
-    Dict with the following entries:
-        'modes' - list of Zernike modes
-        'modesmat' - matrix of (nmodes, npixels)
-        'covmat' - covariance matrix for the generated set of Zernike modes
-        'covmat_in' - inverse of 'covmat'
-        'mask' - binary mask to crop only the orthogonal portions of the calculated modes.
+    tuple: (slopes-to-zernike matrix, zernike-to-slope matrix)
     """
+    x = pup_coords[0]
+    y = pup_coords[1]
+    rho, phi = cart2pol([x, y])
+    zern_slopes = [zernike_slope_noll(zmode, rho, phi) for zmode in range(modestart, nmodes+modestart)]
+    zern_slopes_mat = np.r_[zern_slopes].reshape(nmodes, -1)  # X slopes and then Y slopes for each mode
 
-    if nmodes <= 0:
-        return {'modes': [], 'modesmat': [], 'covmat': 0, 'covmat_in': 0, 'mask': [[0]]}
-    if rad <= 0:
-        raise ValueError("radius should be > 0")
-    if modestart <= 0:
-        raise ValueError("**modestart** Noll index should be > 0")
+    # use SVD to set up optimized conversion matrices
+    U, s, Vh = np.linalg.svd(zern_slopes_mat, full_matrices=False)
 
-    # Use vectors instead of a grid matrix
-    rvec = (np.arange(2.0 * rad) - rad) / rad
-    r0 = rvec.reshape(-1, 1)
-    r1 = rvec.reshape(1, -1)
-    grid_rad = make_radial_matrix(2*rad)
-    grid_ang = np.arctan2(r0, r1)
+    # don't need to trim singular values for reasonable numbers of modes so fit all requested modes.
+    zern_inv_mat = np.dot(Vh.T, np.dot(np.diag(1.0/s), U.T))
 
-    grid_mask = grid_rad <= 1
-
-    # Build list of Zernike modes, these are *not* masked/cropped
-    zern_modes = [zernike_noll(zmode, grid_rad, grid_ang) for zmode in range(modestart, nmodes+modestart)]
-
-    # Convert modes to (nmodes, npixels) matrix
-    zern_modes_mat = np.r_[zern_modes].reshape(nmodes, -1)
-
-    covmat = covmat_in = None
-    if calc_covmat:
-        # Calculate covariance matrix
-        covmat = np.array([[np.sum(zerni * zernj * grid_mask) for zerni in zern_modes] for zernj in zern_modes])
-        # Invert covariance matrix using Moore-Penrose pseudo-inversion
-        covmat_in = np.linalg.pinv(covmat)
-
-    # Create and return dict
-    return {'modes': zern_modes, 'modesmat': zern_modes_mat, 'covmat': covmat, 'covmat_in': covmat_in, 'mask': grid_mask}
-
-
-def calc_slope(im, slopes_inv=None):
-    """
-    Calculate 2D slope of **im**, to be used to calculate unit Zernike
-    influence on a Shack-Hartmann image. If **slopes_inv** is given, use that (2, N) matrix for
-    fitting, otherwise generate and pseudo-invert slopes ourselves.
-
-    An advantage of calculating image slopes in this way is that we do not need to know the derivatives
-    of the basis set we are using. We instead calculate them as mapped into pixel space. This makes it easy
-    to support different basis sets (e.g. mirror bending modes or annular zernikes) without also having to
-    determine the analytic derivatives of the basis sets.  An additional advantage over the previous MMT SHWFS
-    system is that this takes into account the integrated zernike slope across an aperture vs. the instantaneous
-    slope at the aperture center.
-
-    Parameters
-    ----------
-    im: 2D ndarray
-        Image to fit slopes to
-    slopes_inv: ndarray or None (default: None)
-        Pre-computed pseudo-inverted slope matrix to fit with.  Set to None to auto-calculate.
-
-    Returns
-    -------
-    tuple of (axis 0 slope, axis 1 slope)
-    """
-
-    if (slopes_inv is None):
-        slopes = (np.indices(im.shape, dtype=float)/(np.r_[im.shape].reshape(-1, 1, 1))).reshape(2, -1)
-        slopes = np.vstack([slopes, np.ones(slopes.shape[1])])
-        slopes_inv = np.linalg.pinv(slopes)
-
-    return np.dot(im.reshape(1, -1), slopes_inv).ravel()[:2]
-
-
-def calc_influence_matrix(subaps, basis_func=make_zernike_basis, nbasis=20, cntr=None,
-                          rad=-1.0, singval=0.75, subapsize=22.0, pixsize=0.1, modestart=2):
-    """
-    Given a sub-aperture array pattern, calculate a matrix that converts
-    image shift vectors in pixels to basis polynomial amplitudes (default: Zernike)
-    and also its inverse. The parameters **subapsize** and **pixsize** are used for
-    absolute calibration.
-
-    The data returned is a tuple of the following:
-        1. Matrix to compute basis mode amplitudes from image shifts
-        2. Matrix to compute image shifts from basis mode amplitudes
-        3. The set of basis polynomials used
-        4. The extent of the basis polynomials in units of **subaps**
-
-    To calculate the above mentioned matrices, we measure the x, y-slope of all basis modes over
-    all sub-apertures, giving a matrix `basisslopes_mat` that converts slopes for each basis polynomial matrix:
-        subap_slopes_vec = basisslopes_mat . basis_amp_vec
-
-    To obtain pixel shifts inside the sub-images we multiply these slopes in radians/subaperture by
-        sfac = π * ap_width * pix_scale / 206265
-    where ap_width is in pixels, pix_scale is arcsec/pixel, and 1/206265 converts arcsec to radians.
-    The factor of π comes from the normalization of the integral of the basis modes over all radii and angles.
-
-    We then have
-        subap_shift_vec = sfac * basisslopes_mat . basis_amp_vec
-
-    To get the inverse relation, we invert `basisslopes_mat`, giving:
-        basis_amp_vec = (sfac * basisslopes_mat)^-1 . subap_shift_vec
-        basis_amp_vec = basis_inv_mat . subap_shift_vec
-
-    Parameters
-    ----------
-    subaps: list of 4-element list-likes
-        List of subapertures formatted as (low0, high0, low1, high1)
-    basis_func: function reference (default: make_zernike_basis)
-        Function that generates an orthongal set of basis polynomials, e.g. Zernike polynomials.
-    nbasis: int (default: 20)
-        Number of basis modes to model
-    cntr: 2-element list-like or None (default: None)
-        Coordinate to center basis set around. If None, use calculated center of **subaps**.
-    rad: float (default: -1.0)
-        Radius of the aperture to use. If negative, used as fraction **-rad**, otherwise used as radius in pixels.
-    singval: float (default: 1.0)
-        Percentage of singular values to take into account when inverting the matrix. By default don't truncate...
-    subapsize: float (default: 22.0)
-        Size of single Shack-Hartmann sub-aperture in detector pixels
-    pixsize: float (default: 0.1)
-        Detector pixel size in arcseconds
-
-    Returns
-    -------
-    Tuple of (
-        shift to basis polynomial matrix,
-        basis polynomial to shift matrix,
-        basis polynomials used,
-        basis polynomial shape in units of **subaps**
-    )
-    """
-    # we already know pixel size in arcsec so multiply by aperture width and convert to radians.
-    sfac = np.pi * subapsize * pixsize / 206265.
-
-    # Geometry: offset between subap pattern and Zernike modes
-    sasize = np.median(subaps[:, 1::2] - subaps[:, ::2], axis=0).astype(int)
-
-    if cntr is None:
-        cntr = np.mean(subaps[:, ::2], axis=0).astype(int)
-
-    # add 0.5 pixel to account for offset from where pixel is indexed and where its physical center is
-    if rad < 0:
-        pattrad = np.max(np.max(subaps[:, 1::2], 0) - np.min(subaps[:, ::2], 0)) / 2.0
-        rad = int((pattrad * -rad) + 0.5)
-    else:
-        rad = int(rad + 0.5)
-    saoffs = np.around(-cntr + np.r_[[rad, rad]]).astype(int)
-
-    extent = cntr[1]-rad, cntr[1]+rad, cntr[0]-rad, cntr[0]+rad
-    basis = basis_func(nbasis, rad, modestart=modestart)
-
-    slopes = (np.indices(sasize, dtype=float)/(np.r_[sasize].reshape(-1, 1, 1))).reshape(2, -1)
-    slopes = np.vstack([slopes, np.ones(slopes.shape[1])])
-    slopes_inv = np.linalg.pinv(slopes)
-
-    basisslopes = np.r_[
-        [
-            [
-                calc_slope(
-                    base[subap[0]+saoffs[0]:subap[1]+saoffs[0], subap[2]+saoffs[1]:subap[3]+saoffs[1]], slopes_inv=slopes_inv
-                ) for subap in subaps
-            ] for base in basis['modes']
-        ]
-    ].reshape(nbasis, -1)
-
-    # np.linalg.pinv() takes the cutoff wrt the *maximum*, but we want more
-    # flexibility in defining a cut-off. this is why we want to use svd() and not pinv().
-    U, s, Vh = np.linalg.svd(basisslopes * sfac, full_matrices=False)
-
-    # set the rank based on the entropy (http://resources.mpi-inf.mpg.de/d5/teaching/ss13/dmm/slides/03-svd-handout.pdf)
-    s_sq = s**2
-    fk = s_sq/s_sq.sum()
-    entropy = -(1./np.log(len(s))) * np.sum(fk * np.log(fk))
-    #np.savez("svd.npz", s)
-    #nvec = np.argwhere(cums >= singval)[0][0]
-    nvec = np.argwhere(fk.cumsum() >= entropy)[0][0]
-
-    s[nvec+1:] = np.inf
-    basis_inv_mat = np.dot(Vh.T, np.dot(np.diag(1.0/s), U.T))
-
-    return basis_inv_mat, basisslopes*sfac, basis, extent
+    return zern_inv_mat, zern_slopes_mat
 
 
 class ZernikeVector(MutableMapping):
