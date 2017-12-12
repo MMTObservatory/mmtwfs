3a3e2152feb036b6a60beb7a768143ad23c3744a
T. E. Pickering <te.pickering@gmail.com>
2017-12-12 02:19:31 -0700

    test_wfs.py - update binospec tests

--- mmtwfs/tests/test_wfs.py
+++ mmtwfs/tests/test_wfs.py
@@ -108,7 +108,7 @@ def test_bino_analysis():
     results = wfs.measure_slopes(test_file, mode="old_binospec")
     zresults = wfs.fit_wavefront(results)
     testval = int(zresults['zernike']['Z10'].value)
-    assert((testval > 0) & (testval < 10))
+    assert((testval > -40) & (testval < -20))
 
 @cleanup
 def test_newbino_analysis():
@@ -117,7 +117,7 @@ def test_newbino_analysis():
     results = wfs.measure_slopes(test_file, mode="binospec")
     zresults = wfs.fit_wavefront(results)
     testval = int(zresults['zernike']['Z10'].value)
-    assert((testval > 250) & (testval < 260))
+    assert((testval > 340) & (testval < 360))
 
 @cleanup
 def test_too_few_spots():
