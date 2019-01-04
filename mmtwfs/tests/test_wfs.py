aa5b01ee92fd30b87cac5b0e938ee8b12fdd0fcf
T. E. Pickering <te.pickering@gmail.com>
2019-01-04 12:24:35 -0700

    tweak test outputs

--- mmtwfs/tests/test_wfs.py
+++ mmtwfs/tests/test_wfs.py
@@ -71,7 +71,7 @@ def test_mmirs_analysis():
     results = mmirs.measure_slopes(test_file)
     zresults = mmirs.fit_wavefront(results)
     testval = int(zresults['zernike']['Z10'].value)
-    assert((testval > 305) & (testval < 320))
+    assert((testval > 205) & (testval < 220))
 
 @cleanup
 def test_f9_analysis():
@@ -98,7 +98,7 @@ def test_f5_analysis():
     results = f5.measure_slopes(test_file)
     zresults = f5.fit_wavefront(results)
     testval = int(zresults['zernike']['Z10'].value)
-    assert((testval > 100) & (testval < 120))
+    assert((testval > -105) & (testval < -90))
 
 @cleanup
 def test_bino_analysis():
@@ -107,7 +107,7 @@ def test_bino_analysis():
     results = wfs.measure_slopes(test_file, mode="binospec")
     zresults = wfs.fit_wavefront(results)
     testval = int(zresults['zernike']['Z10'].value)
-    assert((testval > 190) & (testval < 210))
+    assert((testval > 180) & (testval < 200))
 
 @cleanup
 def test_too_few_spots():
