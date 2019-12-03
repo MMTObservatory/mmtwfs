465710533bce8f9f6fb7fd4afa8587ff9e7c5aba
T. E. Pickering <te.pickering@gmail.com>
2019-12-03 12:31:41 -0700

    fix fix_csvs script to put stuff in main()

--- scripts/fix_csvs.py
+++ scripts/fix_csvs.py
@@ -5,23 +5,26 @@ from pathlib import Path
 from astropy.io import ascii, fits
 from astropy.table import Column
 
-csv_list = Path(sys.argv[1])
+def main():
+    csv_list = Path(sys.argv[1])
 
-with open(csv_list) as f:
-    csvs = f.readlines()
+    with open(csv_list) as f:
+        csvs = f.readlines()
 
-for csv in csvs:
-    csv_path = csv_list.parent / csv.rstrip()
-    t = ascii.read(str(csv_path))
+    for csv in csvs:
+        csv_path = csv_list.parent / csv.rstrip()
+        t = ascii.read(str(csv_path))
 
-    if not 'exptime' in t.columns:
-        exptimes = []
-        for r in t:
-            with fits.open(csv_path.parent / r['file']) as hdulist:
-                exptime = hdulist[-1].header['EXPTIME']
-                exptimes.append(exptime)
-        c = Column(name='exptime', data=exptimes)
-        t.add_column(c)
-        t.write(csv_path, format="ascii.csv", overwrite=True)
-        print(f"writing {str(csv_path)}")
+        if not 'exptime' in t.columns:
+            exptimes = []
+            for r in t:
+                with fits.open(csv_path.parent / r['file']) as hdulist:
+                    exptime = hdulist[-1].header['EXPTIME']
+                    exptimes.append(exptime)
+            c = Column(name='exptime', data=exptimes)
+            t.add_column(c)
+            t.write(csv_path, format="ascii.csv", overwrite=True)
+            print(f"writing {str(csv_path)}")
 
+if __name__ == "__main__":
+    main()
