fea3d4073d38222563223f04d68c254c54bc7d25
T. E. Pickering <te.pickering@gmail.com>
2023-04-27 17:34:12 -0700

    remove redundant python version checking

--- mmtwfs/__init__.py
+++ mmtwfs/__init__.py
@@ -7,22 +7,3 @@ The ``mmtwfs`` package is a collection of code that for analyzing Shack-Hartmann
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
 from ._astropy_init import *  # noqa
-# ----------------------------------------------------------------------------
-
-# Enforce Python version check during package import.
-# This is the same check as the one at the top of setup.py
-import sys
-from distutils.version import LooseVersion
-
-__minimum_python_version__ = "3.7"
-
-__all__ = []
-
-
-class UnsupportedPythonError(Exception):
-    pass
-
-
-if LooseVersion(sys.version) < LooseVersion(__minimum_python_version__):
-    raise UnsupportedPythonError("mmtwfs does not support Python < {}"
-                                 .format(__minimum_python_version__))
